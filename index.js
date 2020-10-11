5e24eff4104c763bb9da93d323e922da810b88cc
Daniel Koss <danielkoss@Daniels-MacBook-Air.local>
2020-10-11 16:23:52 -0400

    Tweeting text and url

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
 const { Client } = require('@petfinder/petfinder-js');
+const Twit = require('twit');
 
 const petfinderApiKeyLoc = process.env.PETFINDER_DOGBOT_API_KEY_LOC;
 const petfinderSecretLoc = process.env.PETFINDER_DOGBOT_SECRET_LOC;
+
+const twitterKeyLoc = process.env.TWITTER_DOGBOT_API_KEY_LOC;
+const twitterSecretLoc = process.env.TWITTER_DOGBOT_API_SECRET_KEY_LOC;
+const twitterAccessTokenLoc = process.env.TWITTER_DOGBOT_ACCESS_TOKEN_LOC;
+const twitterAccessTokenSecretLoc = process.env.TWITTER_DOGBOT_ACCESS_TOKEN_SECRET_LOC;
 
 const getFromSecretManager = async (client, name) => {
   const [version] = await client.accessSecretVersion({
@@ -31,7 +37,7 @@ const lookingFor = [
   'who wants to give you a kiss!',
   'who wants to sit for you!',
   'who wants a booty rub from you.',
-  'who will jump for joy when they meet you!',
+  'who will jump for joy upon meeting you!',
   'who wants to give you all the cuddles!',
   'who will just warm your heart!',
   'who will bring much joy to your home!',
@@ -48,6 +54,7 @@ const getDoggoText = (doggo) => {
 }
 
 exports.run = async (req, res) => {
+  // Getting the doggo
   const secretManagerClient = new SecretManagerServiceClient();
   const petfinderApiKey = await getFromSecretManager(secretManagerClient, petfinderApiKeyLoc);
   const petfinderSecret = await getFromSecretManager(secretManagerClient, petfinderSecretLoc);
@@ -55,5 +62,49 @@ exports.run = async (req, res) => {
 
   const dogs = await petfinderClient.animal.search({ type: 'dog' });
   const doggo = getRandomArray(getValidDoggos(dogs.data.animals));
-  res.send({text: getDoggoText(doggo), photo: doggo.primary_photo_cropped.medium, url: doggo.url});
+  const doggoResponse = {
+    doggo,
+    text: getDoggoText(doggo),
+    photo: doggo.primary_photo_cropped.medium,
+    url: doggo.url
+  };
+  console.log(doggoResponse); // logging doggoResponse to debug when necessary
+
+  // Making the tweet
+  const consumerKey = await getFromSecretManager(secretManagerClient, twitterKeyLoc);
+  const consumerSecret = await getFromSecretManager(secretManagerClient, twitterSecretLoc);
+  const accessToken = await getFromSecretManager(secretManagerClient, twitterAccessTokenLoc);
+  const accessTokenSecret = await getFromSecretManager(
+    secretManagerClient, twitterAccessTokenSecretLoc,
+  );
+
+  const twitClient = new Twit({
+    consumer_key: consumerKey,
+    consumer_secret: consumerSecret,
+    access_token: accessToken,
+    access_token_secret: accessTokenSecret,
+  });
+
+  const tweetText = `${doggoResponse.doggoText} ${doggoResponse.url}`
+
+  if (!doggoResponse.doggoText) {
+    console.log('no tweet was generated because the doggo text is wrong');
+    res.send({
+      doggo,
+      doggoText: doggoText,
+      photo: doggo.primary_photo_cropped.medium,
+      url: doggo.url
+    });
+  } else if (parsedBody.noExecuteTweet) {
+    res.send({
+      tweetText,
+    });
+  } else {
+    const tweetResponse = await twitClient.post('statuses/update', { status: tweetText });
+
+    res.send({
+      tweetText,
+      tweetResponse,
+    });
+  }
 };
