732ed7057000bbdeb30307f33998eb7adf186068
Daniel Koss <danielkoss@Daniels-MacBook-Air.local>
2020-10-11 13:10:31 -0400

    pulling petfinder results, sending for testing

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
+const { Client } = require('@petfinder/petfinder-js');
+
+const petfinderApiKeyLoc = process.env.PETFINDER_DOGBOT_API_KEY_LOC;
+const petfinderSecretLoc = process.env.PETFINDER_DOGBOT_SECRET_LOC;
+
+const getFromSecretManager = async (client, name) => {
+  const [version] = await client.accessSecretVersion({
+    name: `${name}/versions/latest`,
+  });
+  return version.payload.data.toString();
+};
+
 exports.run = async (req, res) => {
-  res.send('hello world, testing build trigger');
-}
+  const secretManagerClient = new SecretManagerServiceClient();
+  const petfinderApiKey = await getFromSecretManager(secretManagerClient, petfinderApiKeyLoc);
+  const petfinderSecret = await getFromSecretManager(secretManagerClient, petfinderSecretLoc);
+  const petfinderClient = new Client({ apiKey: petfinderApiKey, secret: petfinderSecret });
+
+  const dogs = await petfinderClient.animal.search({ type: 'dog' });
+
+  res.send(dogs.data.animals);
+};
